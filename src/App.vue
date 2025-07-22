2d28b7940673c41b0668bcab699c0d2da4c09c35
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-07-22 23:29:57 +1000

    refactor: restructure routing and views; remove About and HomeView, add Login, Layout, Home, and Category components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
 <script setup></script>
 <template>
-  <el-button type="primary">Primary</el-button>
+  <RouterView />
 </template>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    二级路由
+    <RouterView />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
