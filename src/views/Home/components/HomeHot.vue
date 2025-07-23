dd2561891dcc08000e73544198c25b5a755d9a16
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-07-23 10:57:04 +1000

    feat: lay the ground work from home page. implement category bar using data from pinia

---
+++ src/views/Home/components/HomeHot.vue
@@ -0,0 +1,1 @@
+<template>人气好物</template>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,7 +1,18 @@
+<script setup>
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeHot from "./components/HomeHot.vue";
+import HomeProduct from "./components/HomeProduct.vue";
+</script>
 <template>
-  <div style="height: 2000px">主页</div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
 </template>
-
-<script setup></script>
 
 <style lang="scss" scoped></style>
