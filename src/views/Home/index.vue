8374ed18050e8be7ce5689d6241ec8f6be201465
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-07-23 15:01:48 +1000

    feat: update routing for category navigation and enhance layout with home link

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,6 @@ import HomeBanner from "./components/HomeBanner.vue";
 import HomeNew from "./components/HomeNew.vue";
 import HomeHot from "./components/HomeHot.vue";
 import HomeProduct from "./components/HomeProduct.vue";
-import HomePanel from "./components/HomePanel.vue";
 </script>
 <template>
   <div class="container">
@@ -14,12 +13,6 @@ import HomePanel from "./components/HomePanel.vue";
   <HomeNew />
   <HomeHot />
   <HomeProduct />
-  <!-- <HomePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
-    <div>好物</div>
-  </HomePanel>
-  <HomePanel title="人气推荐" subTitle="人气推荐 好多商品">
-    <div>德武好物</div>
-  </HomePanel> -->
 </template>
 
 <style lang="scss" scoped></style>
