faeff7139c0aa5b005bc5ecd88aff5225e8b8bf8
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-07-23 14:49:16 +1000

    feat: create GoodsItem component for reusable product display and update HomeProduct to utilize it

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -2,6 +2,7 @@
 import HomePanel from "./HomePanel.vue";
 import { getProductAPI } from "@/apis/home";
 import { onMounted, ref } from "vue";
+import GoodsItem from "./GoodsItem.vue";
 const productList = ref([]);
 onMounted(async () => {
   const res = await getProductAPI();
@@ -23,12 +24,7 @@ onMounted(async () => {
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+            <GoodsItem :goods="good" />
           </li>
         </ul>
       </div>
@@ -120,42 +116,6 @@ onMounted(async () => {
         &:nth-child(4n) {
           margin-right: 0;
         }
-      }
-    }
-
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all 0.5s;
-
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
       }
     }
   }
