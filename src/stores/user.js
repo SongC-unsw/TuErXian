76047f754970b708c3e38bd74fe0eaf5a4d68668
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-07-24 19:19:18 +1000

    feat: add pinia-plugin-persistedstate for state persistence in user store

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,12 +2,18 @@ import { ref } from "vue";
 import { defineStore } from "pinia";
 import { loginAPI } from "@/apis/user";
 
-export const useUserStore = defineStore("user", () => {
-  const userInfo = ref({});
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    const userInfo = ref({});
 
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginAPI({ account, password });
-    userInfo.value = res.result;
-  };
-  return { userInfo, getUserInfo };
-});
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginAPI({ account, password });
+      userInfo.value = res.result;
+    };
+    return { userInfo, getUserInfo };
+  },
+  {
+    persist: true,
+  },
+);
