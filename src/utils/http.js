bacaba2defbcd2e9b80661138770c865428b2e5f
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-07-24 19:36:25 +1000

    feat: add authorization token to HTTP request interceptor for user authentication

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import "element-plus/theme-chalk/el-message.css";
 import { ElMessage } from "element-plus";
+import { useUserStore } from "@/stores/user";
 // 创建实例
 const httpInstance = axios.create({
   baseURL: "https://pcapi-xiaotuxian-front-devtest.itheima.net",
@@ -8,13 +9,17 @@ const httpInstance = axios.create({
 });
 
 // 请求拦截器
+
 httpInstance.interceptors.request.use(
   (config) => {
+    const userStore = useUserStore();
+    const token = userStore.userInfo.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  },
+  (e) => Promise.reject(e),
 );
 
 // 响应拦截器
